4ad58d3d217976bb4f1a03c47692f126b35e9b61
Panckoshka <panckoshka@mail.ru>
2022-04-11 23:28:09 +0300

    "second task"

--- src/main/kotlin/differentCommission/DifferentCommission.kt
+++ src/main/kotlin/differentCommission/DifferentCommission.kt
@@ -1,2 +1,31 @@
 package differentCommission
 
+fun main(){
+    print("Введите название карты(MasterCard, Maestro, Мир, Visa, VK Pay): ")
+    val nameCard = readln()
+    print("Ведите сумму перевода(в рублях): ")
+    val payment = readln().toInt()
+
+}
+fun commissionMasterCardMaestro(payment: Int): Int {
+    return (payment.toDouble() * 0.006 + 20).toInt()
+}
+fun commissioMirVisa(payment: Int): Int{
+    return (payment.toDouble() * 0.0075).toInt()
+}
+fun commission(nameCard: String, payment: Int): Int{
+    return when{
+        nameCard == "Мир" && commissioMirVisa(payment) > 35 ->
+            commissioMirVisa(payment)
+        nameCard == "Мир" && commissioMirVisa(payment) > 35 -> 35
+
+        nameCard == "Visa" && commissioMirVisa(payment) > 35 ->
+            commissioMirVisa(payment)
+        nameCard == "Visa" && commissioMirVisa(payment) > 35 -> 35
+
+        nameCard == "VK Pay" -> 0
+
+        else ->commissionMasterCardMaestro(payment)
+    }
+
+}
