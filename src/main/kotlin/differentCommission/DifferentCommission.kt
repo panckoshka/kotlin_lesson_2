273a588c5d1f3eb69b2b2282957327ea11ab0049
Panckoshka <panckoshka@mail.ru>
2022-04-12 14:46:06 +0300

    "second task"

--- src/main/kotlin/differentCommission/DifferentCommission.kt
+++ src/main/kotlin/differentCommission/DifferentCommission.kt
@@ -1,34 +1,107 @@
 package differentCommission
 
 fun main() {
-    print("Введите название карты(MasterCard, Maestro, Мир, Visa, VK Pay): ")
-    val nameCard = readln()
-    print("Ведите сумму перевода(в рублях): ")
-    val payment = readln().toInt()
-    val commission = commission(nameCard, payment)
-    print("Комиссия составит $commission руб.")
+//    print("Введите название карты(MasterCard, Maestro, Мир, Visa, VK Pay): ")
+//    val nameCard = readln()
+//    print("Ведите сумму перевода(минимальный платеж 100 коп): ")
+//    val payment = readln().toInt()
+
+//    val nameCard = "Мир"
+//    val payment = 76_000
+//    val monthPayments = 500
+//
+//    val commissionMasterCard = calcCommission(nameCard, payment, monthPayments)
+//    val commission = calcCommission(nameCard, payment = payment)
+//    val commissionVkPay = calcCommission(payment = payment)
+//
+//    print("Комиссия составит $commissionVkPay коп.")
+//    print("Комиссия составит $commission коп.")
+
+    println(
+        "Кейс 1. Перевод с маэтро 200 руб. Получили комиссию: " +
+                "${calcCommission("Maestro", 200)} коп," +
+                " ожидаем: 0 коп."
+    )
+    println(
+        "Кейс 2. Перевод с Маэстро 200 руб, мы уже перевели 1000 за месяц. Получили комиссию: " +
+                "${calcCommission("Maestro", 200, 1000)} коп, ожидаем: 0 коп."
+    )
+    println(
+        "Кейс 3. Перевод с Маэстро 200 руб, мы уже перевели 80000 за месяц. Получили комиссию: " +
+                "${calcCommission("Maestro", 200, 80000)} коп, ожидаем: 21 руб."
+    )
+
+    println(
+        "Кейс 4. Перевод с Мир 200 руб. Получили комиссию: " +
+                "${calcCommission("Мир", 200)} коп, ожидаем: 35 руб."
+    )
+    println(
+        "Кейс 5. Перевод с Мир 10000 руб. Получили комиссию: " +
+                "${calcCommission("Мир", 10_000)} коп, ожидаем: 75 руб."
+    )
+
+    println(
+        "Кейс 6. Перевод с Vk Pay 200 руб. Получили комиссию: " +
+                "${calcCommission(payment = 200)} коп, ожидаем: 0 коп."
+    )
+    try {
+        println(
+            "Кейс 7. Перевод с Vk Pay 100_000 руб. Получили комиссию: " +
+                    "${calcCommission(payment = 100_000)} коп, ожидаем исключение."
+        )
+
+    } catch (e: Exception) {
+        println("Получили исключение.")
+    }
+
+    try {
+        println(
+            "Кейс 8. Перевод с Маэстро 151_000 руб, мы уже перевели 601_000 за месяц. Получили комиссию: " +
+                    "${calcCommission("Maestro", 151_000, 601_000)} коп," +
+                    " ожидаем: ожидаем исключение"
+        )
+
+    } catch (e: Exception) {
+        println("Получили исключение. Привышены лимиты.")
+    }
+    try {
+        println(
+            "Кейс 9. Перевод с Мир 151_000 руб, мы уже перевели 601_000 за месяц. Получили комиссию: " +
+                    "${calcCommission("Maestro", 151_000, 601_000)} коп," +
+                    " ожидаем: ожидаем исключение"
+        )
+
+    } catch (e: Exception) {
+        println("Получили исключение. Привышены лимиты.")
+    }
 }
 
-fun commissionMasterCardMaestro(payment: Int): Int {
-    return (payment.toDouble() * 0.006 + 20).toInt()
+fun commissionMasterCardMaestro(payment: Int, monthPayments: Int): Int {
+    val commissio = ((payment.toDouble() * 0.006 + 20) * 100).toInt()
+    val transferAmount = payment + monthPayments
+    if (transferAmount > 76_000) {
+        return commissio
+    }
+    return 0
 }
 
-fun commissioMirVisa(payment: Int): Int {
-    return (payment.toDouble() * 0.0075).toInt()
+fun commissioMirVisa(payment: Int, monthPayments: Int): Int {
+    val commissio = ((payment.toDouble() * 0.0075) * 100).toInt()
+    if (commissio > 3500 && payment < 150_000 && monthPayments < 600_000) {
+        return commissio
+    }
+    return 3500
 }
 
-fun commission(nameCard: String, payment: Int): Int {
+fun calcCommission(nameCard: String = "VK Pay", payment: Int, monthPayments: Int = 0): Int {
+
     return when {
-        nameCard == "Мир" && commissioMirVisa(payment) > 35 ->
-            commissioMirVisa(payment)
-        nameCard == "Мир" && commissioMirVisa(payment) < 35 -> 35
+        nameCard == "Visa" || nameCard == "Мир" && payment < 150_000 && monthPayments < 600_000 ->
+            commissioMirVisa(payment, monthPayments)
+        nameCard == "MasterCard" || nameCard == "Maestro" && payment < 150_000 && monthPayments < 600_000 ->
+            commissionMasterCardMaestro(payment, monthPayments)
+        nameCard == "VK Pay" && payment < 15_000 && monthPayments < 40_000 -> 0
 
-        nameCard == "Visa" && commissioMirVisa(payment) > 35 ->
-            commissioMirVisa(payment)
-        nameCard == "Visa" && commissioMirVisa(payment) < 35 -> 35
-
-        nameCard == "VK Pay" -> 0
-
-        else -> commissionMasterCardMaestro(payment)
+        else -> throw Exception("Что-то пошло не так")
     }
 }
